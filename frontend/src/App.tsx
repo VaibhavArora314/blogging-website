534683a373a3e122884d33ed10d1179da07167df
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-07-18 21:08:13 +0530

    added recoil to the frontend and created auth atom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,33 +6,41 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import { NewBlog } from "./pages/NewBlog";
 import Blog from "./pages/Blog";
+import { RecoilRoot } from "recoil";
+import React from "react";
+import Loader from "./components/Loader";
 
 function App() {
   return (
     <BrowserRouter>
-      <>
-        <Routes>
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/*" element={<>
-            <div className="min-h-screen flex flex-col justify-between">
-              <div>
-
-              <Navbar />
-            <Routes>
-              <Route path="/profile/:id" element={<></>} />
-              <Route path="/profile" element={<></>} />
-              <Route path="/new-blog" element={<NewBlog />} />
-              <Route path="/blog/:id" element={<Blog />} />
-              <Route path="/" element={<Home />} />
-            </Routes>
-              </div>
-            <Footer/>
-            </div>
-          </>
-          } />
-        </Routes>
-      </>
+      <RecoilRoot>
+        <React.Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route
+              path="/*"
+              element={
+                <>
+                  <div className="min-h-screen flex flex-col justify-between">
+                    <div>
+                      <Navbar />
+                      <Routes>
+                        <Route path="/profile/:id" element={<></>} />
+                        <Route path="/profile" element={<></>} />
+                        <Route path="/new-blog" element={<NewBlog />} />
+                        <Route path="/blog/:id" element={<Blog />} />
+                        <Route path="/" element={<Home />} />
+                      </Routes>
+                    </div>
+                    <Footer />
+                  </div>
+                </>
+              }
+            />
+          </Routes>
+        </React.Suspense>
+      </RecoilRoot>
     </BrowserRouter>
   );
 }
